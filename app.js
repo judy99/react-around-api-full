74cbcc15d0d9e897fa1d51b1349671ffd3270531
Julia Kurianova <judy@sfdev.com>
2020-10-30 13:10:46 -0400

    Modify app.js

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const { errors } = require('celebrate');
-// const { showError } = require('./utils/showError');
 const error = require('./middlewares/error');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 
@@ -22,32 +21,12 @@ app.use(requestLogger);
 
 const cardsRoute = require('./routes/cards');
 const usersRoute = require('./routes/users');
-// const mid = require('./middlewares/auth.js');
-// temporary workaround. All cards will have the same author.
-// app.use((req, res, next) => {
-//   req.user = {
-//     _id: '5f6a161c35b014788c230f74',
-//   };
-//   next();
-// });
-
-// app.use(mid);
 
 app.use(cardsRoute);
 app.use(usersRoute);
-// app.get('*', (req, res) => {
-//   const e = new Error();
-//   e.name = 'NotFound';
-//   showError(res, e, 'Resource not found');
-// });
-// app.use('*', (req, res) => {
-//   const e = new Error();
-//   showError(res, e, 'Internal Server Error');
-// });
 app.use(errorLogger);
 
 app.use(errors()); // celebrate error handler
-
 app.use(error); // centralized error handler
 
 const {
